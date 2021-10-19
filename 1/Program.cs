b5cf6714b802231fbaeef1690109eaf32bd4be76
YuLiaKhar <julia.harchenko02@gmail.com>
2021-10-19 15:25:52 +0300

    Добавьте файлы проекта.

---
+++ 1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Building building = new Building("Ул. Садовая, д.3", 60, 40, 4);
+            MultiBuilding multiBuilding = new MultiBuilding("Ул. Мраморная, д.8", 40, 30, 20, 5);
+            building.Print();
+            Console.WriteLine();
+            multiBuilding.Print();
+            Console.ReadKey();
+        }
+    }
+    class Building
+    {
+        public string Address { get; set; }
+        public int Length { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Building(string address, int length, int wigth, int height)
+        {
+            Address = address;
+            Length = length;
+            Width = wigth;
+            Height = height;
+        }
+        public void Print()
+        {
+            Console.WriteLine("Адрес: {0}", Address);
+            Console.WriteLine("Длина здания: {0} м", Length);
+            Console.WriteLine("Ширина здания: {0} м", Width);
+            Console.WriteLine("Высота здания: {0} м", Height);
+        }
+    }
+    sealed class MultiBuilding : Building
+    {
+        public int Floor { get; set; }
+        public MultiBuilding(string address, int length, int wigth, int height, int floor) : base(address, length, wigth, height)
+        {
+            Floor = floor;
+        }
+        public new void Print()
+        {
+            base.Print();
+            Console.WriteLine("Количество этажей: {0}" ,Floor);
+        }
+    }
+}
---
+++ 1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("1")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("ae9a52a8-b8ae-40de-8a6e-2fd47c5812da")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
